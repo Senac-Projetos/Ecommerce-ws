c045c775fdbf4a645d1984c42310b8b3f3cb61b2
JoniMarque5 <joni23.marques@gmail.com>
2017-07-06 20:50:12 -0300

    criacao do modelo + geracao do banco de dados para clientes

--- src/main/java/com/bierall/bierallws/BierallWsApplication.java
+++ src/main/java/com/bierall/bierallws/BierallWsApplication.java
@@ -1,9 +1,13 @@
 package com.bierall.bierallws;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableAutoConfiguration
 @SpringBootApplication
+@EnableJpaRepositories
 public class BierallWsApplication {
 
 	public static void main(String[] args) {
