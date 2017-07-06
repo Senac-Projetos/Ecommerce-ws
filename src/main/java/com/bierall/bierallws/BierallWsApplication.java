5e46d9f856e5abd75e16f5a7ea879485ecf69a29
JoniMarque5 <joni23.marques@gmail.com>
2017-07-06 20:01:48 -0300

    criação do esqueleto do projeto back-end

---
+++ src/main/java/com/bierall/bierallws/BierallWsApplication.java
@@ -0,0 +1,12 @@
+package com.bierall.bierallws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BierallWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BierallWsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bierall/bierallws/BierallWsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bierall.bierallws;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BierallWsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
