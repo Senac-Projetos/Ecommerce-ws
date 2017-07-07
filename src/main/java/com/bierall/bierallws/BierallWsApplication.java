f26a6d7d08f72eac321801ac14caccf74b1b150d
JoniMarque5 <joni23.marques@gmail.com>
2017-07-06 21:48:32 -0300

    salvar cliente e endereco

--- src/main/java/com/bierall/bierallws/BierallWsApplication.java
+++ src/main/java/com/bierall/bierallws/BierallWsApplication.java
@@ -3,8 +3,13 @@ package com.bierall.bierallws;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
 @EnableAutoConfiguration
 @SpringBootApplication
 @EnableJpaRepositories
